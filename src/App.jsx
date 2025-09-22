ca0a00c29d79c6ea34924e61242baf6f5ce4e66d
ashfaq <khanashfaq9423@gmail.com>
2025-09-22 12:49:29 +0530

    Ajackus for a user-management Application Assignment complete and update stylin

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import { UserProvider } from './context/UserContext';
-import MainLayout from './layouts/MainLayout';
-import DashboardPage from './pages/DashboardPage';
-import AddUserPage from './pages/AddUserPage';
-import EditUserPage from './pages/EditUserPage';
-import NotFoundPage from './pages/NotFoundPage';
+// src/App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import DashboardPage from "./pages/DashboardPage";
+import AddUserPage from "./pages/AddUserPage";
+import EditUserPage from "./pages/EditUserPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
- <>
- 
-            <Routes>
-      {/* MainLayout parent hoga */}
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<DashboardPage />} />
-        <Route path="add" element={<AddUserPage />} />
-        <Route path="edit/:id" element={<EditUserPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
- </>
-          
-   
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-500">
+      <Routes>
+        {/* MainLayout parent route */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="add" element={<AddUserPage />} />
+          <Route path="edit/:id" element={<EditUserPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
